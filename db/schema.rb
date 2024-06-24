02f4c6c83f7e2e6f00a109da916f0defc65e304e
tama3br0 <newairport180@gmail.com>
2024-06-24 15:10:14 +0900

    faviconを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_22_152658) do
-  create_table "diaries", charset: "utf8mb3", force: :cascade do |t|
+  create_table "diaries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.date "date", null: false
     t.datetime "created_at", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_22_152658) do
     t.index ["user_id"], name: "fk_rails_f03fd03c63"
   end
 
-  create_table "grade_classes", charset: "utf8mb3", force: :cascade do |t|
+  create_table "grade_classes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "grade", null: false
     t.integer "class_num", null: false
     t.integer "school_code", null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_22_152658) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "stamps", charset: "utf8mb3", force: :cascade do |t|
+  create_table "stamps", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "diary_id", null: false
     t.string "stamp_image", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_22_152658) do
     t.index ["user_id"], name: "fk_rails_64f545624f"
   end
 
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "uid", null: false
