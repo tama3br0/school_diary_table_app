1822c7f42e1c46522eeb77f0105c30f81ed80e52
tama3br0 <newairport180@gmail.com>
2024-06-26 22:20:37 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,32 +10,39 @@ class UsersController < ApplicationController
         @user = current_user
         @user.skip_validations = false
 
-        grade_class = GradeClass.find_or_create_by(
-          grade: params[:user][:grade].to_i,
-          class_num: params[:user][:class_num].to_i,
-          school_code: params[:user][:school_code].to_i
-        )
+        begin
+          grade_class = GradeClass.find_or_create_by(
+            grade: params[:user][:grade].to_i,
+            class_num: params[:user][:class_num].to_i,
+            school_code: params[:user][:school_code].to_i
+          )
 
-        @user.grade_class = grade_class
-        @user.assign_attributes(user_params.except(:grade, :class_num, :school_code))
-        @user.additional_info_provided = true
+          @user.grade_class = grade_class
+          @user.assign_attributes(user_params.except(:grade, :class_num, :school_code))
+          @user.additional_info_provided = true
 
-        if @user.valid? && unique_combination?(grade_class, @user.student_num)
-          @user.save
-          respond_to do |format|
-            format.html { redirect_to authenticated_root_path, notice: 'とうろく できました！' }
-            format.turbo_stream { redirect_to authenticated_root_path, notice: 'とうろく できました！' }
+          if @user.valid? && unique_combination?(grade_class, @user.student_num)
+            @user.save
+            respond_to do |format|
+              format.html { redirect_to authenticated_root_path, notice: 'とうろく できました！' }
+              format.turbo_stream { redirect_to authenticated_root_path, notice: 'とうろく できました！' }
+            end
+          else
+            flash.now[:alert] = @user.errors.full_messages.join("\n")
+            flash.now[:alert] = "すでに とうろくされているひとが います" if @user.errors[:student_num].include?("すでに、ほかのひとが とうろく されています")
+
+            respond_to do |format|
+              format.html { render :additional_info }
+              format.turbo_stream {
+                render turbo_stream: turbo_stream.replace("additional_info_form", partial: "users/additional_info_form", locals: { user: @user })
+              }
+            end
           end
-        else
-          flash.now[:alert] = @user.errors.full_messages.join("\n")
-          flash.now[:alert] = "すでに とうろくされているひとが います" if @user.errors[:student_num].include?("すでに、ほかのひとが とうろく されています")
-
-          respond_to do |format|
-            format.html { render :additional_info }
-            format.turbo_stream {
-              render turbo_stream: turbo_stream.replace("additional_info_form", partial: "users/additional_info_form", locals: { user: @user })
-            }
-          end
+        rescue => e
+          Rails.logger.error("Error in save_additional_info: #{e.message}")
+          Rails.logger.error(e.backtrace.join("\n"))
+          flash.now[:alert] = "An unexpected error occurred. Please try again later."
+          render :additional_info
         end
     end
 
