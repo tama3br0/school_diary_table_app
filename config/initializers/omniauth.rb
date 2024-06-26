aa927aa92090c1e31f942344d26c29f51d87aca6
tama3br0 <newairport180@gmail.com>
2024-06-26 21:51:12 +0900

    getを追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ if Rails.env.development?
     OmniAuth.config.silence_get_warning = true
     OmniAuth.config.request_validation_phase = nil
 else
-    OmniAuth.config.allowed_request_methods = %i[post]
+    OmniAuth.config.allowed_request_methods = %i[get post]
     OmniAuth.config.silence_get_warning = false
     OmniAuth.config.request_validation_phase = OmniAuth::AuthenticityTokenProtection.new
 end
